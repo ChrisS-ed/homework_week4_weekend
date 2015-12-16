fd6c334a652f7d85e1df999bd06b98af7a129302
ChrisS-ed <c.sloane@hotmail.co.uk>
2015-12-16 20:18:30 +0000

    added create recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,24 @@ class RecipesController < ApplicationController
         @recipe = Recipe.find(params[:id])
     end
 
+    def new
+        @recipe = Recipe.new
+        @categories = Category.all
+    end
+
+    def create
+       Recipe.create(recipe_params)
+       redirect_to(recipes_path)
+     end
+
+    def edit
+        @recipe = Recipe.find(params[:id])
+    end
+
+    private
+    
+    def recipe_params
+       params.require(:recipe).permit(:name, :description, :instructions, :category_id)
+    end
+
 end
