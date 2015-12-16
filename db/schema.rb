239cf4bb533869eb1a6d441618999399346c25ff
ChrisS-ed <c.sloane@hotmail.co.uk>
2015-12-16 18:27:05 +0000

    can display recipes' categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151215123211) do
+ActiveRecord::Schema.define(version: 20151216172212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20151215123211) do
     t.text     "instructions"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "category_id"
   end
 
 end
